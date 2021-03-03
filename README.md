19c07dcf2e9d4080d6303e0a3d016e77daab70eb
KOSSONTINI Yosri <yosri.kossontini@esprit.tn>
2021-03-03 16:25:51 +0100

    Package hierarchy

--- README.md
+++
@@ -1,1 +0,0 @@
-# JrdnEnft-API
---
+++ kindergarten_project_main/src/main/java/tn/kindergarten/spring/KindergartenProjectMainApplication.java
@@ -0,0 +1,13 @@
+package tn.kindergarten.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenProjectMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindergartenProjectMainApplication.class, args);
+	}
+
+}
